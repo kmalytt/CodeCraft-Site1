2d7b553b39c859a8e94b2427a00e3e498d79b28e
kasha <kurbanmaria377@gmail.com>
2025-12-12 00:56:20 +0200

    2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='../frontend/page')
+app = Flask(__name__, template_folder='frontend/page')
 
 @app.route('/')
 def home():
